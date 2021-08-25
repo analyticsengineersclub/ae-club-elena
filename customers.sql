8cd34c1b5d068eb5120155fd6bff1d7006522068
Ubuntu <ubuntu@ip-172-31-28-161.us-east-2.compute.internal>
2021-08-25 16:41:43 +0000

    calc is back, new comment

--- customers.sql
+++ customers.sql
@@ -1,8 +1,10 @@
 select 
     customers.id as customer_id,
     customers.name,
+    customers.email,
     min(orders.created_at) as first_order_at,
     count(distinct orders.id) as number_of_orders 
+-- a different comment from Elena
 from `analytics-engineers-club.coffee_shop.customers` customers 
 inner join `analytics-engineers-club.coffee_shop.orders` orders 
 on customers.id = orders.customer_id

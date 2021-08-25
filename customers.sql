7f8ebf3a9dfbd60878f5ed6877c5fb57b86b5d2d
Ubuntu <ubuntu@ip-172-31-31-19.us-east-2.compute.internal>
2021-08-25 00:28:44 +0000

    Creating conflict

--- customers.sql
+++ customers.sql
@@ -3,7 +3,8 @@ select
     customers.name,
     customers.email,
     min(orders.created_at) as first_order_at,
-    count(distinct orders.id) as number_of_orders 
+    count(distinct orders.id) as number_of_orders
+    -- Hello! This line should create a merge conflict :) 
 from `analytics-engineers-club.coffee_shop.customers` customers 
 inner join `analytics-engineers-club.coffee_shop.orders` orders 
 on customers.id = orders.customer_id

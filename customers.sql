512878c4a157bc19a6a78d97ece97724682ca235
Elena Dyachkova <dyachkova.elena@gmail.com>
2021-08-26 18:31:00 -0400

    "edit changes"

--- customers.sql
+++ customers.sql
@@ -1,10 +1,6 @@
 select 
     customers.id as customer_id,
-<<<<<<< HEAD
     customers.name as full_name,
-=======
-    customers.name as customer_name,
->>>>>>> 7ab4f4e... other alias
     customers.email,
     min(orders.created_at) as first_order_at,
     count(distinct orders.id) as number_of_orders
